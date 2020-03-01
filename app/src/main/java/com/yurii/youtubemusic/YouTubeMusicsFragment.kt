ec8262cb2de915d7fd94d615c605795587687445
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2020-03-01 20:08:52 +0100

    Implement navigation menu

--- app/src/main/java/com/yurii/youtubemusic/YouTubeMusicsFragment.kt
+++ app/src/main/java/com/yurii/youtubemusic/YouTubeMusicsFragment.kt
@@ -1,22 +1,20 @@
 package com.yurii.youtubemusic
 
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 
-/**
- * A simple [Fragment] subclass.
- */
 class YouTubeMusicsFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+        inflater: LayoutInflater,
+        container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        (activity as AppCompatActivity).supportActionBar!!.title = "YouTube Musics"
+
         return inflater.inflate(R.layout.fragment_you_tube_musics, container, false)
     }
 
