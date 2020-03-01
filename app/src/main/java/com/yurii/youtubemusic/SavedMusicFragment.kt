8985594fa8dedda59248a18a7a4a48d9dd002f30
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2020-03-01 20:06:01 +0100

    Create idle first fragment

---
+++ app/src/main/java/com/yurii/youtubemusic/SavedMusicFragment.kt
@@ -0,0 +1,24 @@
+package com.yurii.youtubemusic
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SavedMusicFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_saved_music, container, false)
+    }
+
+
+}
