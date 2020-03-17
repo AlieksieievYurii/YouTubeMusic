56132a24dedfc65dc6357cf58f4417897bfae7bd
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2020-03-17 15:39:04 +0100

    Create class builder to simplify пуеgetting e data

---
+++ app/src/main/java/com/yurii/youtubemusic/YouTubeService.kt
@@ -0,0 +1,95 @@
+package com.yurii.youtubemusic
+
+import android.os.AsyncTask
+import com.google.api.client.extensions.android.http.AndroidHttp
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
+import com.google.api.client.http.HttpTransport
+import com.google.api.client.json.JsonFactory
+import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.services.youtube.YouTube
+import com.google.api.services.youtube.model.Playlist
+import java.lang.Exception
+import java.lang.IllegalStateException
+
+class YouTubeService {
+    class PlayLists private constructor(
+        private val googleAccountCredential: GoogleAccountCredential?,
+        private val onResult: ((List<Playlist>) -> Unit)?,
+        private val onError: ((error: Exception) -> Unit)?
+    ) {
+
+        fun execute() {
+            googleAccountCredential?.let {
+                PlatListsTask(it, onResult, onError).execute()
+            } ?: throw IllegalStateException("Please set google account credential")
+        }
+
+        private class PlatListsTask(
+            googleAccountCredential: GoogleAccountCredential,
+            val onResult: ((List<Playlist>) -> Unit)?,
+            val onError: ((error: Exception) -> Unit)?
+        ) : AsyncTask<Void, Void, List<Playlist>>() {
+
+            private val service: YouTube
+            private var lastError: Exception? = null
+
+            init {
+                val transport: HttpTransport = AndroidHttp.newCompatibleTransport()
+                val jsonFactory: JsonFactory = JacksonFactory.getDefaultInstance()
+                service = YouTube.Builder(transport, jsonFactory, googleAccountCredential)
+                    .setApplicationName("YouTube Data API PlayLists")
+                    .build()
+            }
+
+            override fun doInBackground(vararg params: Void?): List<Playlist>? {
+                return try {
+                    getPlayLists()
+                } catch (error: Exception) {
+                    lastError = error
+                    cancel(true)
+                    null
+                }
+            }
+
+            override fun onPostExecute(result: List<Playlist>?) {
+                result?.let {
+                    onResult?.invoke(it)
+                }
+            }
+
+            private fun getPlayLists(): List<Playlist> =
+                service.playlists().list("snippet,contentDetails")
+                    .setMine(true)
+                    .setMaxResults(50)
+                    .execute().items
+            //TODO Implement mechanism which obtains all items if results size is more than 50
+
+            override fun onCancelled() {
+                if (lastError != null && onError != null)
+                    onError.invoke(lastError!!)
+            }
+
+        }
+
+        data class Builder(
+            var googleAccountCredential: GoogleAccountCredential? = null,
+            var onResult: ((List<Playlist>) -> Unit)? = null,
+            var onError: ((error: Exception) -> Unit)? = null
+        ) {
+
+            fun googleAccountCredential(googleAccountCredential: GoogleAccountCredential) = apply {
+                this.googleAccountCredential = googleAccountCredential
+            }
+
+            fun onResult(onResult: ((List<Playlist>) -> Unit)) = apply {
+                this.onResult = onResult
+            }
+
+            fun onError(onError: ((error: Exception) -> Unit)?) = apply {
+                this.onError = onError
+            }
+
+            fun build() = PlayLists(googleAccountCredential, onResult, onError)
+        }
+    }
+}
