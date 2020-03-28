8690b3076ee3ec4a738ea8e3c347e4c135739554
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2020-03-28 17:17:58 +0100

    Create the static function to conver duration format(ISO 8601) to a normall view

--- app/src/main/java/com/yurii/youtubemusic/utilities/Utils.kt
+++ app/src/main/java/com/yurii/youtubemusic/utilities/Utils.kt
@@ -10,7 +10,9 @@ import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.client.util.ExponentialBackOff
 import com.google.api.services.youtube.YouTubeScopes
 import com.google.api.services.youtube.model.Playlist
+import org.threeten.bp.Duration
 import java.io.File
+import java.util.concurrent.TimeUnit
 
 const val DEFAULT_SHARED_PREFERENCES_FILE: String = "com.yurii.youtubemusic.shared.preferences"
 const val SHARED_PREFERENCES_SELECTED_PLAY_LIST: String = "com.yurii.youtubemusic.shared.preferences.selected.play.list"
@@ -19,7 +21,7 @@ const val PREF_ACCOUNT_NAME = "accountName"
 class Preferences private constructor() {
     companion object {
         fun setSelectedPlayList(context: Context, playList: Playlist) {
-            val sharedPreferences =  context.getSharedPreferences(DEFAULT_SHARED_PREFERENCES_FILE, Context.MODE_PRIVATE)
+            val sharedPreferences = context.getSharedPreferences(DEFAULT_SHARED_PREFERENCES_FILE, Context.MODE_PRIVATE)
             with(sharedPreferences.edit()) {
                 putString(SHARED_PREFERENCES_SELECTED_PLAY_LIST, playList.toString())
                 apply()
@@ -27,7 +29,7 @@ class Preferences private constructor() {
         }
 
         fun getSelectedPlayList(context: Context): Playlist? {
-            val sharedPreferences = context.getSharedPreferences(DEFAULT_SHARED_PREFERENCES_FILE,Context.MODE_PRIVATE)
+            val sharedPreferences = context.getSharedPreferences(DEFAULT_SHARED_PREFERENCES_FILE, Context.MODE_PRIVATE)
             val jsonRepresentation: String? = sharedPreferences.getString(SHARED_PREFERENCES_SELECTED_PLAY_LIST, null)
             jsonRepresentation?.let {
                 val jsonFactory: com.google.api.client.json.JsonFactory = JacksonFactory.getDefaultInstance()
@@ -82,4 +84,20 @@ class Authorization private constructor() {
         }
     }
 }
+
+class DateTime private constructor() {
+    companion object {
+        fun parseToHumanView(text: String): String {
+            val millis = Duration.parse(text).toMillis()
+            val hours = TimeUnit.MILLISECONDS.toHours(millis)
+            val minutes = TimeUnit.MILLISECONDS.toMinutes(millis) % TimeUnit.HOURS.toMinutes(1)
+            val seconds = TimeUnit.MILLISECONDS.toSeconds(millis) % TimeUnit.MINUTES.toSeconds(1)
+
+            val secondsString = if (seconds < 10) "0$seconds" else seconds.toString()
+
+            return if (hours > 0) "$hours:$minutes:$secondsString" else "$minutes:$secondsString"
+        }
+    }
+}
+
 
--- app/src/test/java/com/yurii/youtubemusic/utilities/DateTimeTest.kt
+++ app/src/test/java/com/yurii/youtubemusic/utilities/DateTimeTest.kt
@@ -1,5 +1,44 @@
 package com.yurii.youtubemusic.utilities
 
 import org.junit.Assert.*
+import org.junit.Test
 
-class DateTimeTest
+class DateTimeTestParsingISO8601TimeFormat {
+
+    @Test
+    fun `Test minutes soconds with zero`() {
+        val isoFormat = "PT9M3S"
+        val minutes = 9
+        val seconds = 3
+
+        assertEquals("$minutes:0$seconds", DateTime.parseToHumanView(isoFormat))
+    }
+
+    @Test
+    fun `Test hours minutes seconds`() {
+        val isoFormat = "PT10H37S"
+        val hours = 10
+        val minutes = 0
+        val seconds = 37
+
+        assertEquals("$hours:$minutes:$seconds", DateTime.parseToHumanView(isoFormat))
+    }
+
+    @Test
+    fun `Test seconds`() {
+        val isoFormat = "PT30S"
+        val minutes = 0
+        val seconds = 30
+
+        assertEquals("$minutes:$seconds", DateTime.parseToHumanView(isoFormat))
+    }
+
+    @Test
+    fun `Test minutes seconds`() {
+        val isoFormat = "PT4M35S"
+        val minutes = 4
+        val seconds = 35
+
+        assertEquals("$minutes:$seconds", DateTime.parseToHumanView(isoFormat))
+    }
+}
