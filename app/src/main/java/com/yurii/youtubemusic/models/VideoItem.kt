35be5af8e74ee1ed3391fca464138c91d206d71c
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2020-03-28 17:09:08 +0100

    Obtain the additional data from a request of video details and put it to VideoItemObject. Also add throwing a exception if to load more videos while selectedplaylist is null

--- app/src/main/java/com/yurii/youtubemusic/models/VideoItem.kt
+++ app/src/main/java/com/yurii/youtubemusic/models/VideoItem.kt
@@ -1,9 +1,13 @@
 package com.yurii.youtubemusic.models
 
 import java.io.Serializable
+import java.math.BigInteger
 
 data class VideoItem(val videoId: String? = null,
                      val title: String? = null,
                      val authorChannelTitle: String? = null,
+                     val description: String? = null,
+                     val duration: String? = null,
+                     val viewCount: BigInteger? = null,
                      val thumbnail: String? = null,
                      var downloadingProgress: Int = 0) : Serializable
