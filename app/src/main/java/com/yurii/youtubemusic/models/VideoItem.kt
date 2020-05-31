6f076e3eaf3e7facdf96a610a889e4531e486497
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2020-05-31 15:46:55 +0200

    Remove progress from VideoItem

--- app/src/main/java/com/yurii/youtubemusic/models/VideoItem.kt
+++ app/src/main/java/com/yurii/youtubemusic/models/VideoItem.kt
@@ -11,8 +11,7 @@ data class VideoItem(val videoId: String? = null,
                      val viewCount: BigInteger? = null,
                      val likeCount: BigInteger? = null,
                      val disLikeCount: BigInteger? = null,
-                     val thumbnail: String? = null,
-                     var downloadingProgress: Int = 0) : Serializable {
+                     val thumbnail: String? = null) : Serializable {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
