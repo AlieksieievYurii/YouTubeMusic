2bd38febd8113552ee753e0c6d4510bc6562bd50
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2020-06-21 13:53:42 +0200

    Add expand/collapse animation

--- app/src/main/java/com/yurii/youtubemusic/videoslist/VideosListAdapter.kt
+++ app/src/main/java/com/yurii/youtubemusic/videoslist/VideosListAdapter.kt
@@ -1,9 +1,15 @@
 package com.yurii.youtubemusic.videoslist
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
+import androidx.core.animation.doOnEnd
+import androidx.core.animation.doOnStart
+import androidx.core.view.doOnLayout
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.yurii.youtubemusic.R
@@ -31,23 +37,28 @@ interface VideoItemInterface {
 class VideosListAdapter(context: Context, private val videoItemInterface: VideoItemInterface) : RecyclerView.Adapter<BaseViewHolder>() {
     companion object {
         private const val NO_POSITION = -1
-        private var expandedPosition = NO_POSITION
     }
 
     val videos: MutableList<VideoItem> = mutableListOf()
-    private var isLoaderVisible: Boolean = false
     private val inflater: LayoutInflater = LayoutInflater.from(context)
+    private lateinit var recyclerView: RecyclerView
+    private var isLoaderVisible: Boolean = false
+    private var expandedPosition = NO_POSITION
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
         return when (viewType) {
             VIEW_TYPE_NORMAL ->
-                VideoViewHolder(DataBindingUtil.inflate(inflater, R.layout.item_video, parent, false)) {
-                    notifyDataSetChanged()
-                }
+                VideoViewHolder(DataBindingUtil.inflate(inflater, R.layout.item_video, parent, false))
             VIEW_TYPE_LOADING ->
                 LoadingViewHolder(DataBindingUtil.inflate<ItemLoadingBinding>(inflater, R.layout.item_loading, parent, false).root)
             else -> throw IllegalStateException("Illegal view type")
         }
+    }
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        this.recyclerView = recyclerView
     }
 
     override fun getItemViewType(position: Int): Int = if (isLoaderVisible)
@@ -89,81 +100,86 @@ class VideosListAdapter(context: Context, private val videoItemInterface: VideoI
 
         val videoItem = videos[position]
         val videoViewHolder = holder as VideoViewHolder
-        videoViewHolder.setOnRemoveClickListener(View.OnClickListener {
-            videoItemInterface.remove(videoItem)
-            videoViewHolder.bind(videoItem, position)
-        })
 
-        videoViewHolder.setOnDownloadClickListener(View.OnClickListener {
-            videoItemInterface.onItemClickDownload(videoItem)
-            videoViewHolder.bind(videoItem, position, state = ItemState.IS_LOADING)
-        })
+        expandItem(videoViewHolder, position == expandedPosition, animate = false)
 
-        videoViewHolder.setOnCancelClickListener(View.OnClickListener {
-            videoItemInterface.cancelDownloading(videoItem)
-            videoViewHolder.bind(videoItem, position)
-        })
+        videoViewHolder.cardContainer.setOnClickListener {
+            when (expandedPosition) {
+                NO_POSITION -> {
+                    expandedPosition = position
+                    expandItem(videoViewHolder, expand = true, animate = true)
+                }
+                position -> {
+                    expandedPosition = NO_POSITION
+                    expandItem(videoViewHolder, expand = false, animate = true)
+                }
+                else -> {
+                    val expandedItem = recyclerView.findViewHolderForLayoutPosition(expandedPosition) as? VideoViewHolder
+                    if (expandedItem != null)
+                        expandItem(expandedItem, expand = false, animate = true)
+
+                    expandItem(videoViewHolder, expand = true, animate = true)
+                    expandedPosition = position
+                }
+            }
+         }
+
+//        videoViewHolder.setOnRemoveClickListener(View.OnClickListener {
+//            videoItemInterface.remove(videoItem)
+//            videoViewHolder.bind(videoItem, position)
+//        })
+//
+//        videoViewHolder.setOnDownloadClickListener(View.OnClickListener {
+//            videoItemInterface.onItemClickDownload(videoItem)
+//            videoViewHolder.bind(videoItem, position, state = ItemState.IS_LOADING)
+//        })
+//
+//        videoViewHolder.setOnCancelClickListener(View.OnClickListener {
+//            videoItemInterface.cancelDownloading(videoItem)
+//            videoViewHolder.bind(videoItem, position)
+//        })
 
         when {
-            videoItemInterface.isExisted(videoItem) -> videoViewHolder.bind(videoItem, position, state = ItemState.EXISTS)
+            videoItemInterface.isExisted(videoItem) -> videoViewHolder.setData(videoItem, state = ItemState.EXISTS)
             videoItemInterface.isLoading(videoItem) -> {
-                videoViewHolder.bind(
+                videoViewHolder.setData(
                     videoItem,
-                    position,
                     progress = videoItemInterface.getCurrentProgress(videoItem),
                     state = ItemState.IS_LOADING
                 )
             }
-            else -> videoViewHolder.bind(videoItem, position, state = ItemState.DOWNLOAD)
+            else -> videoViewHolder.setData(videoItem, state = ItemState.DOWNLOAD)
         }
     }
 
-    class VideoViewHolder(val videoItemVideoBinding: ItemVideoBinding, private val onItemChange: ((position: Int) -> Unit)) :
-        BaseViewHolder(videoItemVideoBinding.root) {
-        private var isExpanded = false
+    private fun expandItem(view: VideoViewHolder, expand: Boolean, animate: Boolean) {
+        view.expandableLayout.measure(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        val expandedItemHeight: Int = view.expandableLayout.measuredHeight
 
-        fun bind(videoItem: VideoItem, position: Int, progress: Progress? = null, state: ItemState = ItemState.DOWNLOAD) {
+        if (animate) {
+            val animator = getValueAnimator(expand, 200L, AccelerateDecelerateInterpolator()) { setExpandedProgress(view, expandedItemHeight, it) }
+
+            if (expand) animator.doOnStart { view.expandableLayout.isVisible = true }
+            else animator.doOnEnd { view.expandableLayout.isVisible = false }
+
+            animator.start()
+
+        } else setExpandedProgress(view, expandedItemHeight, if (expand) 1f else 0f)
+    }
+
+    private fun setExpandedProgress(view: VideoViewHolder, expandedHeight: Int, progress: Float) {
+        view.expandableLayout.layoutParams.height = if (progress == 1f) ViewGroup.LayoutParams.WRAP_CONTENT else (expandedHeight * progress).toInt()
+        view.expandableLayout.requestLayout()
+    }
+
+    class VideoViewHolder(val videoItemVideoBinding: ItemVideoBinding) : BaseViewHolder(videoItemVideoBinding.root) {
+        val cardContainer: View = videoItemVideoBinding.cardContainer
+        val expandableLayout: View = videoItemVideoBinding.expandableLayout
+        fun setData(videoItem: VideoItem, progress: Progress? = null, state: ItemState = ItemState.DOWNLOAD) =
             videoItemVideoBinding.apply {
                 this.videoItem = videoItem
                 this.state = state
                 this.progress = progress
-
-                if (position == expandedPosition)
-                    expandDetails().also { isExpanded = true }
-                else
-                    collapseDetails().also { isExpanded = false }
-
-                this.root.setOnClickListener {
-                    expandedPosition = if (isExpanded) NO_POSITION else position
-                    if (expandedPosition == NO_POSITION)
-                        collapseDetails().also { isExpanded = false; onItemChange.invoke(position) }
-                    else
-                        expandDetails().also { isExpanded = true; onItemChange.invoke(position) }
-                }
             }.executePendingBindings()
-        }
-
-
-        private fun expandDetails() {
-            videoItemVideoBinding.detailsPartLayout.visibility = View.VISIBLE
-            // TODO Need to add some expanding animation
-        }
-
-        private fun collapseDetails() {
-            videoItemVideoBinding.detailsPartLayout.visibility = View.GONE
-            // TODO Need to add some collapsing animation
-        }
-
-        fun setOnDownloadClickListener(onClickListener: View.OnClickListener) {
-            videoItemVideoBinding.download.setOnClickListener(onClickListener)
-        }
-
-        fun setOnRemoveClickListener(onClickListener: View.OnClickListener) {
-            videoItemVideoBinding.remove.setOnClickListener(onClickListener)
-        }
-
-        fun setOnCancelClickListener(onClickListener: View.OnClickListener) {
-            videoItemVideoBinding.cancelButton.setOnClickListener(onClickListener)
-        }
     }
 }
