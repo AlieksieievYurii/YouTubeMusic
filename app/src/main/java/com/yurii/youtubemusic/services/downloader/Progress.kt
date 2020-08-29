b05ea5575977f4e5bd7f2036edd6bc2840aee98e
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2020-08-29 16:17:37 +0200

    Add the function update

--- app/src/main/java/com/yurii/youtubemusic/services/downloader/Progress.kt
+++ app/src/main/java/com/yurii/youtubemusic/services/downloader/Progress.kt
@@ -8,9 +8,21 @@ import java.io.Serializable
  * [currentSize] - Current loaded size in bytes. [currentSizeInMb] returns [currentSize] in megabytes
  * [totalSize] - Total size of downloading item in bytes. [totalSizeInMb] returns [totalSize] in megabytes
  */
-data class Progress(val progress: Int, val currentSize: Int, val totalSize: Int) : Serializable {
+data class Progress(var progress: Int, var currentSize: Int, var totalSize: Int) : Serializable {
     val currentSizeInMb: Double
         get() = currentSize.toDouble() / 1000_000
     val totalSizeInMb: Double
         get() = totalSize.toDouble() / 1000_000
+
+    fun update(progress: Int, currentSize: Int, totalSize: Int) {
+        this.progress = progress
+        this.currentSize = currentSize
+        this.totalSize = totalSize
+    }
+
+    companion object {
+        fun create(): Progress {
+            return Progress(0, 0, 0)
+        }
+    }
 }
