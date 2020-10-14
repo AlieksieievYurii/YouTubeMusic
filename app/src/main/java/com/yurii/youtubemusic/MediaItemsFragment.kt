fa0bf45fb0c3f17a1a69a402c89b8bce5dc05eca
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2020-10-14 14:32:42 +0200

    Add loading bar

--- app/src/main/java/com/yurii/youtubemusic/MediaItemsFragment.kt
+++ app/src/main/java/com/yurii/youtubemusic/MediaItemsFragment.kt
@@ -6,11 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
+import androidx.core.view.isVisible
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.yurii.youtubemusic.databinding.FragmentMediaItemsBinding
 import com.yurii.youtubemusic.mediaservice.PLAYBACK_STATE_MEDIA_ITEM
 import com.yurii.youtubemusic.models.Category
 import com.yurii.youtubemusic.models.MediaMetaData
@@ -22,20 +25,22 @@ import com.yurii.youtubemusic.viewmodels.mediaitems.MediaItemsViewModel
 class MediaItemsFragment : Fragment() {
     private lateinit var viewModel: MediaItemsViewModel
     private lateinit var mediaItemsAdapterController: MediaListAdapterController
+    private lateinit var binding: FragmentMediaItemsBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val root = inflater.inflate(R.layout.fragment_media_items, container, false)
-        val mediaItemsRecyclerView: RecyclerView = root.findViewById(R.id.media_items)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_media_items, container, false)
         val category: Category = requireArguments().getParcelable(EXTRA_CATEGORY)!!
-        initRecyclerView(mediaItemsRecyclerView)
+        initRecyclerView(binding.mediaItems)
         initViewModel(category)
-        return root
+        return binding.root
     }
 
     private fun initViewModel(category: Category) {
         viewModel = Injector.provideMediaItemsViewModel(requireContext(), category)
         viewModel.mediaItems.observe(viewLifecycleOwner, Observer {
             mediaItemsAdapterController.setMediaItems(it)
+            binding.loadingBar.isVisible = false
+            binding.mediaItems.isVisible = true
         })
 
         viewModel.playbackState.observe(viewLifecycleOwner, Observer {
