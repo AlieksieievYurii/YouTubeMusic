c00fac984da134a04ffcdf32f7f88f9abc2cce0f
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2020-11-21 12:46:06 +0100

    Create the data class that represenrs audio effects data. Also there is the factory method to init first values

---
+++ app/src/main/java/com/yurii/youtubemusic/models/AudioEffectsData.kt
@@ -0,0 +1,40 @@
+package com.yurii.youtubemusic.models
+
+import android.media.audiofx.Equalizer
+
+
+data class AudioEffectsData(
+    var enableEqualizer: Boolean,
+    var enableBassBoost: Boolean,
+    var enableVirtualizer: Boolean,
+    var numberOfBands: Short,
+    var lowestBandLevel: Short,
+    var highestBandLevel: Short,
+    var bands: ArrayList<Int>,
+    var bandsLevels: HashMap<Int, Int>,
+    var bassBoost: Int,
+    var virtualizer: Int
+) {
+    companion object {
+        fun create(): AudioEffectsData {
+            val globalEqualizer = Equalizer(0, 0)
+            val listOfCenterFreq = ArrayList<Int>()
+            (0 until globalEqualizer.numberOfBands).map { globalEqualizer.getCenterFreq(it.toShort()) }.mapTo(listOfCenterFreq) { it / 1000 }
+            val bandsLevels = HashMap<Int, Int>()
+            val max = globalEqualizer.bandLevelRange.last() + globalEqualizer.bandLevelRange.first()
+            (0 until globalEqualizer.numberOfBands).forEach { bandsLevels[it] = max }
+            return AudioEffectsData(
+                enableEqualizer = false,
+                enableBassBoost = false,
+                enableVirtualizer = false,
+                numberOfBands = globalEqualizer.numberOfBands,
+                lowestBandLevel = globalEqualizer.bandLevelRange.first(),
+                highestBandLevel = globalEqualizer.bandLevelRange.last(),
+                bands = listOfCenterFreq,
+                bandsLevels = bandsLevels,
+                bassBoost = 0,
+                virtualizer = 0
+            )
+        }
+    }
+}
