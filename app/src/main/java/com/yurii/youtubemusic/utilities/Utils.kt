90ece076a4cf3225a287536ded2b3ebea3d1ea56
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-02-14 19:50:37 +0100

    Move access to application's preferences to a separated class wich implement interface

--- app/src/main/java/com/yurii/youtubemusic/utilities/Utils.kt
+++ app/src/main/java/com/yurii/youtubemusic/utilities/Utils.kt
@@ -5,71 +5,12 @@ import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
-import com.google.api.client.json.jackson2.JacksonFactory
-import com.google.api.services.youtube.model.Playlist
-import com.google.gson.Gson
 import com.yurii.youtubemusic.R
-import com.yurii.youtubemusic.models.AudioEffectsData
-import com.yurii.youtubemusic.models.Category
 import org.threeten.bp.Duration
 import java.io.File
 import java.math.BigInteger
 import java.util.*
 import java.util.concurrent.TimeUnit
-
-const val DEFAULT_SHARED_PREFERENCES_FILE: String = "com.yurii.youtubemusic.shared.preferences"
-const val SHARED_PREFERENCES_SELECTED_PLAY_LIST: String = "com.yurii.youtubemusic.shared.preferences.selected.play.list"
-const val SH_KEY_MUSICS_CATEGORIES: String = "com.yurii.youtubemusic.shared.preferences.music.categories"
-const val SH_KEY_AUDIO_EFFECTS_DATA: String = "com.yurii.youtubemusic.shared.preferences.audio.effects.data"
-
-object Preferences {
-
-    fun getAudioEffectsData(context: Context): AudioEffectsData {
-        val sharedPreferences = context.getSharedPreferences(DEFAULT_SHARED_PREFERENCES_FILE, Context.MODE_PRIVATE)
-        val audioEffectsDataJson = sharedPreferences.getString(SH_KEY_AUDIO_EFFECTS_DATA, null) ?: return AudioEffectsData.create()
-        return Gson().fromJson(audioEffectsDataJson, AudioEffectsData::class.java)
-    }
-
-    fun setAudioEffectsData(context: Context, audioEffectsData: AudioEffectsData) {
-        val sharedPreferences = context.getSharedPreferences(DEFAULT_SHARED_PREFERENCES_FILE, Context.MODE_PRIVATE)
-        with(sharedPreferences.edit()) {
-            putString(SH_KEY_AUDIO_EFFECTS_DATA, Gson().toJson(audioEffectsData))
-            apply()
-        }
-    }
-
-    fun getMusicCategories(context: Context): List<Category> {
-        val sharedPreferences = context.getSharedPreferences(DEFAULT_SHARED_PREFERENCES_FILE, Context.MODE_PRIVATE)
-        val categoriesJson: String = sharedPreferences.getString(SH_KEY_MUSICS_CATEGORIES, null) ?: return emptyList()
-        return Gson().fromJson(categoriesJson, Array<Category>::class.java).toList()
-    }
-
-    fun setCategories(context: Context, categories: List<Category>) {
-        val sharedPreferences = context.getSharedPreferences(DEFAULT_SHARED_PREFERENCES_FILE, Context.MODE_PRIVATE)
-        with(sharedPreferences.edit()) {
-            putString(SH_KEY_MUSICS_CATEGORIES, Gson().toJson(categories))
-            apply()
-        }
-    }
-
-    fun setSelectedPlayList(context: Context, playList: Playlist?) {
-        val sharedPreferences = context.getSharedPreferences(DEFAULT_SHARED_PREFERENCES_FILE, Context.MODE_PRIVATE)
-        with(sharedPreferences.edit()) {
-            putString(SHARED_PREFERENCES_SELECTED_PLAY_LIST, playList?.toString())
-            apply()
-        }
-
-    }
-
-    fun getSelectedPlayList(context: Context): Playlist? {
-        val sharedPreferences = context.getSharedPreferences(DEFAULT_SHARED_PREFERENCES_FILE, Context.MODE_PRIVATE)
-        val jsonRepresentation: String? = sharedPreferences.getString(SHARED_PREFERENCES_SELECTED_PLAY_LIST, null)
-        jsonRepresentation?.let {
-            val jsonFactory: com.google.api.client.json.JsonFactory = JacksonFactory.getDefaultInstance()
-            return jsonFactory.fromString(it, Playlist::class.java)
-        } ?: return null
-    }
-}
 
 class ErrorSnackBar private constructor() {
     companion object {
