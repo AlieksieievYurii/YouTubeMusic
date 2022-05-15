59b4ef275019bb9e4f147988eefa23794b5a03c2
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2022-05-15 21:35:25 +0200

    Move the VideoItem to Youtube package

---
+++ app/src/main/java/com/yurii/youtubemusic/screens/youtube/models/VideoItem.kt
@@ -0,0 +1,36 @@
+package com.yurii.youtubemusic.screens.youtube.models
+
+import com.google.api.services.youtube.model.Video
+import java.io.Serializable
+import java.math.BigInteger
+
+data class VideoItem(
+    val videoId: String,
+    val title: String,
+    val authorChannelTitle: String,
+    val description: String,
+    val duration: String,
+    val viewCount: BigInteger,
+    val likeCount: BigInteger,
+    val thumbnail: String,
+    val normalThumbnail: String
+) : Serializable {
+
+    companion object {
+        fun createFrom(video: Video): VideoItem =
+            VideoItem(
+                videoId = video.id,
+                title = video.snippet.title,
+                description = video.snippet.description,
+                duration = video.contentDetails.duration,
+                viewCount = video.statistics.viewCount,
+                likeCount = video.statistics.likeCount,
+                authorChannelTitle = video.snippet.channelTitle,
+                thumbnail = video.snippet.thumbnails.default.url,
+                normalThumbnail = video.snippet.thumbnails.medium.url
+            )
+
+        fun createMock(): VideoItem =
+            VideoItem("", "", "", "", "", BigInteger.ZERO, BigInteger.ZERO, "", "")
+    }
+}
