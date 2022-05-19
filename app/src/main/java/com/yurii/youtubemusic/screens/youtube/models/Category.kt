c4b9655d5541c0bc6155b4359be09a2b0922189c
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2022-05-19 17:59:42 +0200

    Move Category class to YouTube screen package

---
+++ app/src/main/java/com/yurii/youtubemusic/screens/youtube/models/Category.kt
@@ -0,0 +1,25 @@
+package com.yurii.youtubemusic.screens.youtube.models
+
+import android.os.Parcelable
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaDescriptionCompat
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Category(val id: Int, val name: String) : Parcelable {
+    companion object {
+        val ALL = Category(1, "all")
+
+        fun createFrom(mediaItem: MediaBrowserCompat.MediaItem): Category {
+            return Category(mediaItem.mediaId!!.toInt(), mediaItem.description.title.toString())
+        }
+    }
+}
+
+fun Category.toMediaItem(): MediaBrowserCompat.MediaItem {
+    val mediaDescription = MediaDescriptionCompat.Builder()
+        .setMediaId(this.id.toString())
+        .setTitle(this.name)
+        .build()
+    return MediaBrowserCompat.MediaItem(mediaDescription, MediaBrowserCompat.MediaItem.FLAG_BROWSABLE)
+}
