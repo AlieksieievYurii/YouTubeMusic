c6d3b972d7c5315ff4fe53e3b35cd2a5abb4b921
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2022-12-29 20:25:26 +0100

    Add unit test for MediaLibraryManager.changeMediaItemPosition

--- app/src/test/java/com/yurii/youtubemusic/services/media/MediaLibraryManagerTest.kt
+++ app/src/test/java/com/yurii/youtubemusic/services/media/MediaLibraryManagerTest.kt
@@ -1,5 +1,7 @@
 package com.yurii.youtubemusic.services.media
 
+import com.yurii.youtubemusic.models.Category
+import com.yurii.youtubemusic.models.CategoryContainer
 import com.yurii.youtubemusic.models.MediaItem
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.launch
@@ -14,6 +16,7 @@ import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 import java.io.File
 
 
@@ -28,6 +31,16 @@ class MediaLibraryManagerTest {
         "description",
         File("./thumbnail.jpg"),
         File("./media.mp3")
+    )
+
+    private val mediaItemTwo = MediaItem(
+        "id2",
+        "title2",
+        "author2",
+        123L,
+        "description2",
+        File("./thumbnail2.jpg"),
+        File("./media2.mp3")
     )
 
     @Mock
@@ -48,13 +61,32 @@ class MediaLibraryManagerTest {
 
         runTest(UnconfinedTestDispatcher()) {
             val job = launch {
-                 mediaLibraryManager.event.collect { events.add(it) }
+                mediaLibraryManager.event.collect { events.add(it) }
             }
 
             mediaLibraryManager.deleteItem(mediaItem)
             verify(mediaStorage, times(1)).eliminateMediaItem(mediaItem.id)
             job.cancel()
         }
-        assertEquals(listOf(MediaLibraryManager.Event.ItemDeleted(mediaItem)),events)
+        assertEquals(listOf(MediaLibraryManager.Event.ItemDeleted(mediaItem)), events)
+    }
+
+    @Test
+    fun changeMediaItemPosition_itemsExist_changed() = runTest(UnconfinedTestDispatcher()) {
+        val mockedCategoryContainer = CategoryContainer(Category.ALL, listOf(mediaItem.id, mediaItemTwo.id))
+        whenever(mediaStorage.getCategoryContainer(Category.ALL)).thenReturn(mockedCategoryContainer)
+
+        val events = mutableListOf<MediaLibraryManager.Event>()
+
+        val job = launch {
+            mediaLibraryManager.event.collect { events.add(it) }
+        }
+
+        mediaLibraryManager.changeMediaItemPosition(Category.ALL, mediaItem, 0, 1)
+        job.cancel()
+
+        val newCategoryContainer = CategoryContainer(Category.ALL, listOf(mediaItemTwo.id, mediaItem.id)) // Changed order
+        verify(mediaStorage, times(1)).saveCategoryContainer(newCategoryContainer)
+        assertEquals(listOf(MediaLibraryManager.Event.MediaItemPositionChanged(Category.ALL, mediaItem, 0, 1)), events)
     }
 }
