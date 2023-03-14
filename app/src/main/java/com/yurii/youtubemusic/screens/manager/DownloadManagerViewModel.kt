b2213dff93e6b7070f231a6fc0af24986aa49429
AlieksieievYurii <alieksieiev.yurii@gmail.com>
2023-03-14 16:37:39 +0100

    Define(experimentaly) two data classes for playlistbinds and downloading jobs

--- app/src/main/java/com/yurii/youtubemusic/screens/manager/DownloadManagerViewModel.kt
+++ app/src/main/java/com/yurii/youtubemusic/screens/manager/DownloadManagerViewModel.kt
@@ -4,6 +4,9 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+data class PlaylistSyncBind(val playlistName: String)
+data class DownloadingJob(val videoItemName: String, val videoItemId: String, val thumbnail: String)
+
 @HiltViewModel
 class DownloadManagerViewModel @Inject constructor() : ViewModel() {
 }
