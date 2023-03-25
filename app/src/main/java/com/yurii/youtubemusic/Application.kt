328f25b668683393ef52ab73df6e1834b8065ba6
AlieksieievYurii <alieksieiev.yurii@gmail.com>
2023-03-25 18:26:56 +0100

    Remove uneccessary stuff from the application class

--- app/src/main/java/com/yurii/youtubemusic/Application.kt
+++ app/src/main/java/com/yurii/youtubemusic/Application.kt
@@ -7,10 +7,7 @@ import android.content.Context
 import android.os.Build
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.*
-import com.yurii.youtubemusic.source.Validator
 import dagger.hilt.android.HiltAndroidApp
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -18,20 +15,12 @@ import javax.inject.Inject
 class Application : Application(), Configuration.Provider {
 
     @Inject
-    lateinit var validator: Validator
-
-    @Inject
     lateinit var workerFactory: HiltWorkerFactory
-
-    @Inject
-    lateinit var workManager: WorkManager
 
 
     override fun onCreate() {
         super.onCreate()
         createMediaPlayerNotificationChannel()
-        createMusicsDownloadingNotificationChannel()
-        //validateMediaItems()
 
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
@@ -40,13 +29,6 @@ class Application : Application(), Configuration.Provider {
 
     override fun getWorkManagerConfiguration(): Configuration {
         return Configuration.Builder().setWorkerFactory(workerFactory).build()
-    }
-
-    private fun validateMediaItems() {
-        MainScope().launch {
-            validator.removeDownloadingMocks()
-            validator.validateMediaItems()
-        }
     }
 
     private fun createMediaPlayerNotificationChannel() {
@@ -61,20 +43,7 @@ class Application : Application(), Configuration.Provider {
         }
     }
 
-    private fun createMusicsDownloadingNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = "YoutubeMusicDownloaderNotification"
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(YOUTUBE_DOWNLOADER_NOTIFICATION_CHANNEL, name, importance).apply {
-                description = "Notification description"
-            }
-            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
-    }
-
     companion object {
         const val MEDIA_PLAYER_NOTIFICATION_CHANNEL = "com.yurii.youtubemusic.media.player.notification"
-        const val YOUTUBE_DOWNLOADER_NOTIFICATION_CHANNEL = "com.yurii.youtubemusic.youtube.downloader.notification"
     }
 }
