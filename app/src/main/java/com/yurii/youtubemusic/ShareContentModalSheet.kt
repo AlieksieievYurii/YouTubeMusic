02a2ecc64c0d8fde34aa26a927bf7d43dd25f4fb
AlieksieievYurii <alieksieiev.yurii@gmail.com>
2023-03-28 15:40:45 +0200

    Add SheetDialog to share QR code of selected media item

---
+++ app/src/main/java/com/yurii/youtubemusic/ShareContentModalSheet.kt
@@ -0,0 +1,46 @@
+package com.yurii.youtubemusic
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentManager
+import coil.load
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.yurii.youtubemusic.models.MediaItem
+import com.yurii.youtubemusic.ui.toPx
+import de.hdodenhof.circleimageview.CircleImageView
+import net.glxn.qrgen.android.QRCode
+
+class ShareContentModalSheet : BottomSheetDialogFragment() {
+    lateinit var mediaItem: MediaItem
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.sheet_share_content, container, false)
+
+        val quCode = QRCode.from("${YOUTUBE_WATCH_URL}${mediaItem.id}")
+            .withSize(view.toPx(250), view.toPx(250))
+            .withColor(ContextCompat.getColor(requireContext(), R.color.colorPrimary), 0)
+            .bitmap()
+
+        view.findViewById<ImageView>(R.id.qr_code).setImageBitmap(quCode)
+        view.findViewById<TextView>(R.id.title).text = mediaItem.title
+        view.findViewById<CircleImageView>(R.id.thumbnail).load(mediaItem.thumbnail)
+
+        return view
+    }
+
+    companion object {
+        private const val TAG = "ShareContentModalSheet"
+        private const val YOUTUBE_WATCH_URL = "https://www.youtube.com/watch?v="
+
+        fun show(mediaItem: MediaItem, fragmentManager: FragmentManager) {
+            val instance = ShareContentModalSheet()
+            instance.mediaItem = mediaItem
+            instance.show(fragmentManager, TAG)
+        }
+    }
+}
