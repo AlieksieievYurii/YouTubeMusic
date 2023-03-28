0e7574f7a2897f175ce55e240c70b471b71bb939
AlieksieievYurii <alieksieiev.yurii@gmail.com>
2023-03-28 17:09:11 +0200

    Implement sharing media file

--- app/src/main/java/com/yurii/youtubemusic/ShareContentModalSheet.kt
+++ app/src/main/java/com/yurii/youtubemusic/ShareContentModalSheet.kt
@@ -1,6 +1,7 @@
 package com.yurii.youtubemusic
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,13 +10,16 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.fragment.app.FragmentManager
 import coil.load
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.button.MaterialButton
 import com.yurii.youtubemusic.models.MediaItem
 import com.yurii.youtubemusic.ui.toPx
 import de.hdodenhof.circleimageview.CircleImageView
 import net.glxn.qrgen.android.QRCode
+import timber.log.Timber
 
 class ShareContentModalSheet : BottomSheetDialogFragment() {
     lateinit var mediaItem: MediaItem
@@ -31,21 +35,45 @@ class ShareContentModalSheet : BottomSheetDialogFragment() {
         view.findViewById<ImageView>(R.id.qr_code).setImageBitmap(quCode)
         view.findViewById<TextView>(R.id.title).text = mediaItem.title
         view.findViewById<CircleImageView>(R.id.thumbnail).load(mediaItem.thumbnail)
-        view.findViewById<CardView>(R.id.content).setOnClickListener { openShareSheet() }
+        view.findViewById<CardView>(R.id.content).setOnClickListener { openShareSheetToShareURL() }
+        view.findViewById<MaterialButton>(R.id.share_file).setOnClickListener { openShareSheetToShareMediaFile() }
 
         return view
     }
 
-    private fun openShareSheet() {
+    private fun openShareSheetToShareMediaFile() {
+        val fileUri: Uri? = try {
+            FileProvider.getUriForFile(requireContext(), requireActivity().packageName, mediaItem.mediaFile)
+        } catch (e: IllegalArgumentException) {
+            Timber.e("The selected file can't be shared: ${mediaItem.mediaFile}")
+            null
+        }
+
+        if (fileUri != null) {
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_STREAM, fileUri)
+                type = "audio/*"
+            }
+
+            val shareIntent = Intent.createChooser(sendIntent, getString(R.string.label_share_media_file))
+            sendIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+
+            startActivity(shareIntent)
+        }
+    }
+
+    private fun openShareSheetToShareURL() {
         val sendIntent: Intent = Intent().apply {
             action = Intent.ACTION_SEND
             putExtra(Intent.EXTRA_TEXT, buildUrl())
             type = "text/plain"
         }
 
-        val shareIntent = Intent.createChooser(sendIntent, getString(R.string.label_share_video))
+        val shareIntent = Intent.createChooser(sendIntent, getString(R.string.label_share_video_url))
         startActivity(shareIntent)
     }
+
     private fun buildUrl(): String = "${YOUTUBE_WATCH_URL}${mediaItem.id}"
 
     companion object {
