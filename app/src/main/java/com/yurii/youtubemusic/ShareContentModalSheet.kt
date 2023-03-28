39abfbdc92d861b1ab9d0ca90d6c81ad0920ec6d
AlieksieievYurii <alieksieiev.yurii@gmail.com>
2023-03-28 16:13:31 +0200

    Implement sharing the url

--- app/src/main/java/com/yurii/youtubemusic/ShareContentModalSheet.kt
+++ app/src/main/java/com/yurii/youtubemusic/ShareContentModalSheet.kt
@@ -1,11 +1,13 @@
 package com.yurii.youtubemusic
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentManager
 import coil.load
@@ -21,7 +23,7 @@ class ShareContentModalSheet : BottomSheetDialogFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.sheet_share_content, container, false)
 
-        val quCode = QRCode.from("${YOUTUBE_WATCH_URL}${mediaItem.id}")
+        val quCode = QRCode.from(buildUrl())
             .withSize(view.toPx(250), view.toPx(250))
             .withColor(ContextCompat.getColor(requireContext(), R.color.colorPrimary), 0)
             .bitmap()
@@ -29,9 +31,22 @@ class ShareContentModalSheet : BottomSheetDialogFragment() {
         view.findViewById<ImageView>(R.id.qr_code).setImageBitmap(quCode)
         view.findViewById<TextView>(R.id.title).text = mediaItem.title
         view.findViewById<CircleImageView>(R.id.thumbnail).load(mediaItem.thumbnail)
+        view.findViewById<CardView>(R.id.content).setOnClickListener { openShareSheet() }
 
         return view
     }
+
+    private fun openShareSheet() {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, buildUrl())
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, getString(R.string.label_share_video))
+        startActivity(shareIntent)
+    }
+    private fun buildUrl(): String = "${YOUTUBE_WATCH_URL}${mediaItem.id}"
 
     companion object {
         private const val TAG = "ShareContentModalSheet"
