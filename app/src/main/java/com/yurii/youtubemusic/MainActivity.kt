ca020dd269e9adc945e1b4bc82f49fa9d9e24201
AlieksieievYurii <alieksieiev.yurii@gmail.com>
2023-05-03 18:47:51 +0200

    Add slide animation for Saved & Youtube fragments

--- app/src/main/java/com/yurii/youtubemusic/MainActivity.kt
+++ app/src/main/java/com/yurii/youtubemusic/MainActivity.kt
@@ -3,12 +3,19 @@ package com.yurii.youtubemusic
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.viewbinding.library.activity.viewBinding
 import androidx.activity.viewModels
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.forEach
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
@@ -20,19 +27,18 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import java.lang.IllegalStateException
-
-//TODO (Add enter & exit animation)
+import java.lang.ref.WeakReference
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(), ToolBarAccessor {
     private val viewModel: MainActivityViewModel by viewModels()
     private val activityMainBinding: ActivityMainBinding by viewBinding()
-    private val navController by lazy { (supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment).navController }
+    internal val navController by lazy { (supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment).navController }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setSupportActionBar(activityMainBinding.toolbar)
-        activityMainBinding.bottomNavigationView.setupWithNavController(navController)
+        setupWithNavController()
         activityMainBinding.toolbar.setupWithNavController(
             navController, AppBarConfiguration(setOf(R.id.fragment_youtube_music, R.id.fragment_saved_music))
         )
@@ -71,6 +77,56 @@ class MainActivity : AppCompatActivity(), ToolBarAccessor {
             }
 
         }
+    }
+
+    private fun setupWithNavController() {
+        //activityMainBinding.bottomNavigationView.setupWithNavController(navController)
+        val builder = NavOptions.Builder().setLaunchSingleTop(true).setRestoreState(true)
+        activityMainBinding.bottomNavigationView.setOnItemSelectedListener { item ->
+            if (navController.currentDestination?.id == item.itemId)
+                return@setOnItemSelectedListener false
+
+            when (item.itemId) {
+                R.id.fragment_saved_music -> {
+                    builder.setEnterAnim(R.anim.slide_from_left_to_center)
+                        .setExitAnim(R.anim.slide_from_center_to_right)
+                        .setPopEnterAnim(R.anim.slide_from_right_to_center)
+                        .setPopExitAnim(R.anim.slide_from_center_to_left)
+                }
+
+                R.id.fragment_youtube_music -> {
+                    builder.setEnterAnim(R.anim.slide_from_right_to_center)
+                        .setExitAnim(R.anim.slide_from_center_to_left)
+                        .setPopEnterAnim(R.anim.slide_from_left_to_center)
+                        .setPopExitAnim(R.anim.slide_from_center_to_right)
+                }
+            }
+            if (item.order and Menu.CATEGORY_SECONDARY == 0) {
+                builder.setPopUpTo(
+                    navController.graph.findStartDestination().id,
+                    inclusive = false,
+                    saveState = true
+                )
+            }
+            navController.navigate(item.itemId, null, builder.build())
+            true
+        }
+        val weakReference = WeakReference(activityMainBinding.bottomNavigationView)
+        navController.addOnDestinationChangedListener(
+            object : NavController.OnDestinationChangedListener {
+                override fun onDestinationChanged(controller: NavController, destination: NavDestination, arguments: Bundle?) {
+                    val view = weakReference.get()
+                    if (view == null) {
+                        navController.removeOnDestinationChangedListener(this)
+                        return
+                    }
+                    view.menu.forEach { item ->
+                        if (destination.hierarchy.any { it.id == item.itemId }) {
+                            item.isChecked = true
+                        }
+                    }
+                }
+            })
     }
 
     companion object {
